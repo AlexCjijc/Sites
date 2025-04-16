efed9ced5382a5cfacda47e0b9eafa878ba29122
AlexCjijc <147518298+AlexCjijc@users.noreply.github.com>
2025-04-17 02:48:34 +0700

    webSite

---
+++ frontendWebTT/js/script.js
@@ -0,0 +1,30 @@
+// "Липкий" хедер
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
+// Анимация появления при прокрутке
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Вызов reveal для отображения элементов при загрузке страницы
+reveal();
